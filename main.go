ed0e5c9b0c70fbaaec9a649f8eaadb7c6dd5fa57
Kamol Mavlonov <kamoljan@gmail.com>
2022-07-31 16:10:07 +0900

    chore: update readme

--- main.go
+++ main.go
@@ -2,83 +2,25 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
+	"github.com/microservices-today/microservice-geoip/db"
+	"github.com/microservices-today/microservice-geoip/handlers"
+	"github.com/oschwald/geoip2-golang"
 	"log"
-	"net"
 	"net/http"
 	"os"
-
-	"github.com/oschwald/geoip2-golang"
-	"strings"
 )
-
-var db *geoip2.Reader
 
 func main() {
 	var err error
-	db, err = geoip2.Open("GeoLite2-Country.mmdb")
+	db.Reader, err = geoip2.Open("GeoLite2-Country.mmdb")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-	http.HandleFunc("/country", country) // e.g. /country?ip=81.2.69.142
-	http.HandleFunc("/ip", ip)
-	http.HandleFunc("/outbound", outbound)
-	http.HandleFunc("/health", health)
+	defer db.Reader.Close()
+
+	http.HandleFunc("/country", handlers.Country) // e.g. /country?ip=81.2.69.142
+	http.HandleFunc("/ip", handlers.Ip)
+	http.HandleFunc("/outbound", handlers.Outbound)
+	http.HandleFunc("/health", handlers.Health)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
-}
-
-func health(w http.ResponseWriter, r *http.Request) {
-	// A very simple health check.
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	// In the future we could report back on the status of our DB, or our cache
-	// (e.g. Redis) by performing a simple PING, and include them in the response.
-	io.WriteString(w, `Don't worry, I'm healthy!`)
-}
-
-func ip(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, "GET only", http.StatusMethodNotAllowed)
-		return
-	}
-	// Assuming format is as expected
-	ips := strings.Split(r.Header.Get("X-Forwarded-For"), ", ")
-	for _, ip := range ips {
-		w.Write([]byte(ip + "\n"))
-	}
-}
-
-func outbound(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("https://ifconfig.me")
-	if err != nil {
-		http.Error(w, "Something is wrong with ifconfig.me", http.StatusBadRequest)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	w.Write(body)
-}
-
-func country(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, "GET only", http.StatusMethodNotAllowed)
-		return
-	}
-	qIP := r.URL.Query().Get("ip")
-	if len(qIP) == 0 {
-		http.Error(w, "IP is missing", http.StatusBadRequest)
-		return
-	}
-	ip := net.ParseIP(qIP)
-	record, err := db.Country(ip)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if ip != nil {
-		io.WriteString(w, record.Country.IsoCode)
-	} else {
-		http.Error(w, "IP not found", http.StatusNotFound)
-	}
 }
